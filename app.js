95dadcfebfdf4313751a9df35668b343d63f6c24
Simon Muñoz S <abiatar.ms@gmail.com>
2023-10-27 10:12:13 -0300

    feat: add get endpoint to retrieve data from json

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
 let app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello world!');
+const movies = JSON.parse(fs.readFileSync("./data/movies.json"));
+
+// GET - api/movies
+app.get("/api/v1/movies", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    count: movies.length,
+    data: {
+      movies: movies, // enveloping: wrap and object inside another object
+    },
+  });
 });
 
 app.listen(port, () => {
-  console.log('server running!');
+  console.log("server running!");
 });
