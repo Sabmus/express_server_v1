5a7a94f69ad67d6db860e2695284f98a5c47cb16
Simon Muñoz S <abiatar.ms@gmail.com>
2023-10-27 10:37:47 -0300

    feat: add post endpoint

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const fs = require("fs");
 let app = express();
-
 const port = 3000;
 
 const movies = JSON.parse(fs.readFileSync("./data/movies.json"));
+
+//middleware to read json from request
+app.use(express.json());
 
 // GET - api/movies
 app.get("/api/v1/movies", (req, res) => {
@@ -14,6 +16,22 @@ app.get("/api/v1/movies", (req, res) => {
     data: {
       movies: movies, // enveloping: wrap and object inside another object
     },
+  });
+});
+
+// POST - api/v1/movies
+app.post("/api/v1/movies", (req, res) => {
+  const newId = movies[movies.length - 1].id + 1;
+  const newMovie = Object.assign({ id: newId }, req.body);
+  movies.push(newMovie);
+
+  fs.writeFile("./data/movies.json", JSON.stringify(movies), () => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        movie: newMovie,
+      },
+    });
   });
 });
 
