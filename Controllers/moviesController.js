1be4d04bb626a1bed1025eab08408573a191d4b1
sabmus <abiatar.ms@gmail.com>
2023-11-01 18:07:14 -0300

    refactor: created asyncErrorHandler.js

--- Controllers/moviesController.js
+++ Controllers/moviesController.js
@@ -1,7 +1,8 @@
-const Movie = require("../Models/movieModel");
-const ApiFeatures = require("../utils/ApiFeatures");
+const Movie = require('../Models/movieModel');
+const ApiFeatures = require('../utils/ApiFeatures');
+const asyncErrorHandler = require('../utils/asyncErrorHandler');
 
-const validateReqBody = (req, res, next) => {
+/* const validateReqBody = (req, res, next) => {
   if (!req.body.name || !req.body.duration) {
     return res.status(400).json({
       status: "failed",
@@ -9,186 +10,138 @@ const validateReqBody = (req, res, next) => {
     });
   }
   next();
-};
+}; */
 
 const top5highestRated = (req, res, next) => {
-  req.query.limit = "5";
-  req.query.sort = "-ratings";
+  req.query.limit = '5';
+  req.query.sort = '-ratings';
 
   next();
 };
 
-const getMovieStats = async (req, res) => {
-  try {
-    const stats = await Movie.aggregate([
-      { $match: { ratings: { $gte: 4.5 } } },
-      {
-        $group: {
-          _id: "$releaseYear",
-          avgRating: { $avg: "$ratings" },
-          avgPrice: { $avg: "$price" },
-          minPrice: { $min: "$price" },
-          maxPrice: { $max: "$price" },
-          priceTotal: { $sum: "$price" },
-          movieCount: { $sum: 1 },
-        },
+const getMovieStats = asyncErrorHandler(async (req, res, next) => {
+  const stats = await Movie.aggregate([
+    { $match: { ratings: { $gte: 4.5 } } },
+    {
+      $group: {
+        _id: '$releaseYear',
+        avgRating: { $avg: '$ratings' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' },
+        priceTotal: { $sum: '$price' },
+        movieCount: { $sum: 1 },
       },
-      { $sort: { avgRating: 1 } },
-    ]);
+    },
+    { $sort: { avgRating: 1 } },
+  ]);
 
-    res.status(200).json({
-      status: "success",
-      count: stats.length,
-      data: {
-        stats,
+  res.status(200).json({
+    status: 'success',
+    count: stats.length,
+    data: {
+      stats,
+    },
+  });
+});
+
+const getMovieByGenre = asyncErrorHandler(async (req, res, next) => {
+  const genre = req.params.genre;
+  const movies = await Movie.aggregate([
+    { $unwind: '$genres' },
+    {
+      $group: {
+        _id: '$genres',
+        movieCount: { $sum: 1 },
+        movies: { $push: '$name' },
       },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "failed",
-      message: error.message,
-    });
-  }
-};
+    },
+    { $addFields: { genre: '$_id' } },
+    { $project: { _id: 0 } },
+    { $sort: { movieCount: -1 } },
+    { $match: { genre: genre } },
+    //{ $limit: 3 },
+  ]);
 
-const getMovieByGenre = async (req, res) => {
-  try {
-    const genre = req.params.genre;
-    const movies = await Movie.aggregate([
-      { $unwind: "$genres" },
-      {
-        $group: {
-          _id: "$genres",
-          movieCount: { $sum: 1 },
-          movies: { $push: "$name" },
-        },
-      },
-      { $addFields: { genre: "$_id" } },
-      { $project: { _id: 0 } },
-      { $sort: { movieCount: -1 } },
-      { $match: { genre: genre } },
-      //{ $limit: 3 },
-    ]);
+  res.status(200).json({
+    status: 'success',
+    count: movies.length,
+    data: {
+      movies,
+    },
+  });
+});
 
-    res.status(200).json({
-      status: "success",
-      count: movies.length,
-      data: {
-        movies,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "failed",
-      message: error.message,
-    });
-  }
-};
-
-const getAllMovies = async (req, res) => {
-  try {
-    /**query strings */
-    /* mongoose 6.0 or lower
+const getAllMovies = asyncErrorHandler(async (req, res, next) => {
+  /**query strings */
+  /* mongoose 6.0 or lower
     const excludeFields = ['sort', 'page', 'limit', 'fields'];
     const queryObj = {...req.query};
     excludeFields.forEach(el => delete queryObj[el]);
     const movies = await Movie.find(queryObj);
     */
-    /* mongoose 7.0 or greater
+  /* mongoose 7.0 or greater
     const movies = await Movie.find(req.query);
     */
 
-    const features = new ApiFeatures(Movie.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate(); // Movie.find() return a query object
-    let movies = await features.query;
+  const features = new ApiFeatures(Movie.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate(); // Movie.find() return a query object
+  let movies = await features.query;
 
-    res.status(200).json({
-      status: "success",
-      count: movies.length,
-      data: {
-        movies,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "failed",
-      message: error.message,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    count: movies.length,
+    data: {
+      movies,
+    },
+  });
+});
 
-const getOneMovie = async (req, res) => {
-  try {
-    const movie = await Movie.findById(req.params.id);
+const getOneMovie = asyncErrorHandler(async (req, res, next) => {
+  const movie = await Movie.findById(req.params.id);
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        movie,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
-};
-
-const asyncErrorHandler = (func) => {
-  return (req, res, next) => {
-    func(req, res, next).catch((err) => next(err));
-  };
-};
-
-const createMovie = asyncErrorHandler(async (req, res) => {
-  const movie = await Movie.create(req.body);
-
-  res.status(201).json({
-    status: "success",
+  res.status(200).json({
+    status: 'success',
     data: {
       movie,
     },
   });
 });
 
-const updateMovie = async (req, res) => {
-  try {
-    const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: "success",
-      data: {
-        movie,
-      },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
-};
+const createMovie = asyncErrorHandler(async (req, res, next) => {
+  const movie = await Movie.create(req.body);
 
-const deleteMovie = async (req, res) => {
-  try {
-    await Movie.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: "success",
-      data: null,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
-};
+  res.status(201).json({
+    status: 'success',
+    data: {
+      movie,
+    },
+  });
+});
+
+const updateMovie = asyncErrorHandler(async (req, res, next) => {
+  const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      movie,
+    },
+  });
+});
+
+const deleteMovie = asyncErrorHandler(async (req, res, next) => {
+  await Movie.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
 module.exports = {
   getAllMovies,
@@ -196,7 +149,7 @@ module.exports = {
   createMovie,
   updateMovie,
   deleteMovie,
-  validateReqBody,
+  //validateReqBody,
   top5highestRated,
   getMovieStats,
   getMovieByGenre,
--- Routes/moviesRoutes.js
+++ Routes/moviesRoutes.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const {
   getAllMovies,
@@ -6,17 +6,17 @@ const {
   createMovie,
   updateMovie,
   deleteMovie,
-  validateReqBody,
+  //validateReqBody,
   top5highestRated,
   getMovieStats,
   getMovieByGenre,
-} = require("../Controllers/moviesController");
+} = require('../Controllers/moviesController');
 
-router.route("/hightest-rated").get(top5highestRated, getAllMovies);
-router.route("/stats").get(getMovieStats);
-router.route("/movies-by-genres/:genre").get(getMovieByGenre);
+router.route('/hightest-rated').get(top5highestRated, getAllMovies);
+router.route('/stats').get(getMovieStats);
+router.route('/movies-by-genres/:genre').get(getMovieByGenre);
 
-router.route("/").get(getAllMovies).post(validateReqBody, createMovie);
-router.route("/:id").get(getOneMovie).patch(updateMovie).delete(deleteMovie);
+router.route('/').get(getAllMovies).post(createMovie);
+router.route('/:id').get(getOneMovie).patch(updateMovie).delete(deleteMovie);
 
 module.exports = router;
