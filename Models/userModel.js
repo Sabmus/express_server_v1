64f3751d4ca48de2780151f888123a12d2745b8e
Simon Muñoz S <abiatar.ms@gmail.com>
2023-11-08 12:49:37 -0300

    feat:added password hashing

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,27 +1,28 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const { hashPassword } = require("../utils/hash");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'name is a required field.'],
+    required: [true, "name is a required field."],
   },
   email: {
     type: String,
-    required: [true, 'email is a required field.'],
+    required: [true, "email is a required field."],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, 'must enter a valid email.'],
+    validate: [validator.isEmail, "must enter a valid email."],
   },
   photo: String,
   password: {
     type: String,
-    required: [true, 'password is a required field.'],
+    required: [true, "password is a required field."],
     minlength: 8,
   },
   confirmPassword: {
     type: String,
-    required: [true, 'confirm password is a required field.'],
+    required: [true, "confirm password is a required field."],
     validate: {
       // this validate will work only with save() and create()
       validator: function (val) {
@@ -32,6 +33,17 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  // encrypt password before save it
+  this.password = await hashPassword(this.password);
+  this.confirmPassword = undefined;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
