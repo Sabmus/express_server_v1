e5516289cb8f190498fb27872def94f7f3622b24
Sabmus <abiatar.ms@gmail.com>
2023-12-08 00:36:11 -0300

    refactor: remove sequelize imports

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-const sequelize = require('./utils/dbConn');
-const Transaction = require('./Models/transactionModel');
-const Role = require('./Models/roleModel');
 const dotenv = require('dotenv');
 dotenv.config({ path: '.env' });
 
@@ -16,31 +13,13 @@ const app = require('./app');
 console.log(`currently in: ${app.get('env')} environment`);
 
 const startDB = async () => {
-  try {
-    await sequelize.authenticate();
-    // permanent hook that validates updates to not be able to change UserIds
-    sequelize.addHook('beforeSave', model => {
-      if (model.UserId) {
-        if (model.changed('UserId') && !model.isNewRecord) {
-          model.UserId = model.previous('UserId');
-        }
-      }
-    });
-
-    await sequelize.sync({ force: true });
-    await Role.create({
-      name: 'Usuario',
-    });
-    console.log('\n\nConnection has been established successfully.\n\n');
-  } catch (error) {
-    console.log('\n\nUnable to connect to the database: ', error.message);
-  }
+  console.log('emmpty func');
 };
 
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, async () => {
-  await startDB();
+  //await startDB();
   console.log(process.env.TZ);
   console.log('server running!!');
 });
